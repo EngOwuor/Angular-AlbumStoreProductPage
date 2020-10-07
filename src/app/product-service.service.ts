18b40bb7a70c69058b0baad128155a0149c448ea
Owuor Robert <owuorrobert76@gmail.com>
2020-10-07 21:25:47 +0300

    Completed module

--- src/app/product-service.service.ts
+++ src/app/product-service.service.ts
@@ -1,8 +1,20 @@
+import {Http, Response} from '@angular/http'
 import { Injectable } from '@angular/core';
+import { from } from 'rxjs/observable/from';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
 
 @Injectable()
-export class ProductServiceService {
+export class ProductService {
+  private _albumUrl = '../assets/album.json' 
 
-  constructor() { }
+  constructor(private _http : Http) { }
+
+  getAlbum(id : number){
+     return this._http.get(this._albumUrl).pipe(map(responce=>responce.json));
+  }
+
 
 }
+
